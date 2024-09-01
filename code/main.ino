5565b863d54856131c876ec73bb48911f7ebf509
Thomas <mail@lokalnetz.com>
2024-09-01 09:25:04 +0200

    Initial commit

---
+++ code/main.ino
@@ -0,0 +1,15 @@
+// 
+// Copyright (c) 2021 Thomas Meschke
+//
+// MIT License
+// https://opensource.org/licenses/mit-license.php
+//
+
+void setup() {
+
+}
+
+void loop() {
+
+
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Maker Project Template
+
+`git clone git@github.com:vlrmprjct/maker-project-template.git mynewproject && cd mynewproject && code .`
